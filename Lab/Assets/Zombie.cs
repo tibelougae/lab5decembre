2eecffa545e33900f60224bc525374ce44579dfd
Sebastien Rousseau <sebastien.rousseau.9@ulaval.ca>
2017-12-04 18:57:32 -0500

    Added Zombie

---
+++ Lab/Assets/Zombie.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Zombie : MonoBehaviour {
+
+    private NavMeshAgent agent;
+    private Transform target;
+
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	void FixedUpdate () {
+        agent.SetDestination(target.position);
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.tag == "Player")
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
